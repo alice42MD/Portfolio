3fa460f919eac168c8415834d5b0ed564246785f
Alice42MD <alicemartindevin@gmail.com>
2025-02-12 22:34:27 +0100

    head/next meta view port

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import localFont from "next/font/local"
 import { AlertProvider } from "./ui/alert/alertContext"
 import { ThemeProvider } from "next-themes"
 import { Analytics } from "@vercel/analytics/react"
+import Head from "next/head"
 
 const nostalgia = localFont({
   src: [{ path: "./ui/fonts/16-bit-7x9-nostalgia.ttf", weight: "400" }],
@@ -22,6 +23,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <Head>
+        <meta name="viewport" content="initial-scale=1, width=device-width" />
+      </Head>
       <body className={`${nostalgia.variable} antialiased`}>
         <ThemeProvider attribute={"class"}>
           <AlertProvider>{children}</AlertProvider>
