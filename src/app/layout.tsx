a3c215373968ff3cf865583f8e51ce2eed2b2c75
Alice42MD <alicemartindevin@gmail.com>
2025-02-12 12:34:57 +0100

    use Next ThemeProvider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next"
 import "./ui/globals.css"
 import localFont from "next/font/local"
 import { AlertProvider } from "./ui/alert/alertContext"
+import { ThemeProvider } from "next-themes"
 
 const nostalgia = localFont({
   src: [{ path: "./ui/fonts/16-bit-7x9-nostalgia.ttf", weight: "400" }],
@@ -21,7 +22,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${nostalgia.variable} antialiased`}>
-        <AlertProvider>{children}</AlertProvider>
+        <ThemeProvider>
+          <AlertProvider>{children}</AlertProvider>
+        </ThemeProvider>
       </body>
     </html>
   )
--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -2,30 +2,23 @@
 
 import Link from "next/link"
 import Background from "./ui/background"
-import { useState, useEffect } from "react"
+import ErrorIcon from "./ui/svg/errorIcon"
 
 export default function NotFound() {
-  const [theme, setTheme] = useState<string>("")
-
-  useEffect(() => {
-    const selectedTheme = localStorage.getItem("theme")
-    setTheme(selectedTheme ?? "dark")
-  }, [])
-
-  console.log(theme)
   return (
-    theme && (
-      <div className={`${theme}`}>
-        <Background />
-        <div className="flex flex-col justify-center items-center h-screen">
-          <div>
-            <h1>Not found – 404!</h1>
-          </div>
-          <div>
-            <Link href="/">Go back to Home</Link>
-          </div>
+    <div className="font-[family-name:var(--font-nostalgia)] text-[17px] ">
+      <Background />
+      <div className="relative flex flex-col justify-center items-center h-screen z-10 drop-shadow-shadow">
+        <div>
+          <ErrorIcon className={"fill-primary"} />
+        </div>
+        <div>
+          <h1>Not found – 404!</h1>
+        </div>
+        <div>
+          <Link href="/">Go back to Home</Link>
         </div>
       </div>
-    )
+    </div>
   )
 }
